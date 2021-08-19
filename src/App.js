ecd3ca4b537e145f7106f3aae1e8f082b0c54e09
Guoping Wu <gwu@Guopings-MacBook-Pro.local>
2021-08-18 21:04:02 -0400

    useRef as a mutable container

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Counter from "./Counter";
 import MyList from "./MyList";
+import MyList2 from "./MyList2";
 
 function App() {
   return (
     <div className="App">
       <Counter />
       <MyList />
+      <MyList2 />
     </div>
   );
 }
---
+++ src/MyList2.js
@@ -0,0 +1,45 @@
+import React, { useRef, useEffect } from 'react';
+
+
+function MyList2() {
+    //console.log("MyList2")
+    const listRef = useRef([]);
+    let [value, setValue] = React.useState ('')    
+    let [listed, setListed] = React.useState ([]) 
+
+    const myChangeHandler = (event) => {
+        setValue(event.target.value)
+    };
+
+    const mySubmitHandler = (event) => {
+        //console.log("mySubmitHandler")
+        event.preventDefault();
+        listRef.current.push(value)
+        setListed([...listRef.current])
+    };
+
+    let mlist = listed
+    return (
+      <div className="MyList">
+            <h1>This is MyList Component</h1>
+            <form>
+                <p>Enter your value:</p>
+                <input
+                type="text"
+                name="data"
+                value={value}
+                onChange={myChangeHandler}
+                />
+                <button onClick={mySubmitHandler}>
+                    Submit
+                </button>
+            </form>
+            <div>LIST</div>
+            {mlist && mlist.length >0 && mlist.map ((l,index)=> { 
+                return <div key ={index}> {l} </div>
+            })}
+      </div>
+    );
+  }
+  
+  export default MyList2;
